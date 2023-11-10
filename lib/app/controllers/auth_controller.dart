93a71a105682ac063dff2a8350e564410824b24e
AlexFlorenco <aflorenco13@gmail.com>
2023-11-10 20:06:26 -0300

    feat: Token validity check

--- lib/app/controllers/auth_controller.dart
+++ lib/app/controllers/auth_controller.dart
@@ -1,5 +1,5 @@
 import 'package:docpost/app/controllers/database/user_secure_storage.dart';
-
+import 'dart:convert';
 import '../repositories/auth_repository.dart';
 
 class AuthController {
@@ -8,9 +8,13 @@ class AuthController {
 
   Future<bool> login(String email, String password) async {
     var token = await authRepository.authenticationAPI(
-        email: email, password: password);
+      email: email,
+      password: password,
+    );
+
     if (token != null) {
-      UserSecureStorage().saveToken(token);
+      var expires_in = parseJwt(token)['expires_in'].toString();
+      UserSecureStorage().saveToken(token, expires_in);
       return true;
     } else {
       return false;
@@ -19,11 +23,47 @@ class AuthController {
 
   Future<bool> isAuthenticated() async {
     var token = await storage.getToken();
-    if (token != null) {
-      print('Usuário autenticado!');
+    if (token.isNotEmpty &&
+        int.parse(token['expires_in']!) >
+            DateTime.now().millisecondsSinceEpoch / 1000) {
+      // print('Usuário autenticado!');
       return true;
     }
-    print('Usuário não autenticado!');
+    // print('Usuário não autenticado!');
     return false;
   }
+}
+
+Map<String, dynamic> parseJwt(String token) {
+  final parts = token.split('.');
+  if (parts.length != 3) {
+    throw Exception('Token inválido!');
+  }
+
+  final payload = _decodeBase64(parts[1]);
+  final payloadMap = json.decode(payload);
+  if (payloadMap is! Map<String, dynamic>) {
+    throw Exception('Payload inválido!');
+  }
+
+  return payloadMap;
+}
+
+_decodeBase64(String str) {
+  String output = str.replaceAll('-', '+').replaceAll('_', '/');
+
+  switch (output.length % 4) {
+    case 0:
+      break;
+    case 2:
+      output += '==';
+      break;
+    case 3:
+      output += '=';
+      break;
+    default:
+      throw Exception('Illegal base64url string!"');
+  }
+
+  return utf8.decode(base64Url.decode(output));
 }
--- lib/app/controllers/database/user_secure_storage.dart
+++ lib/app/controllers/database/user_secure_storage.dart
@@ -1,13 +1,14 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
-class UserSecureStorage{
+class UserSecureStorage {
   final _storage = FlutterSecureStorage();
 
-  saveToken(String token) async {
+  saveToken(String token, String expires_in) async {
     await _storage.write(key: 'token', value: token);
+    await _storage.write(key: 'expires_in', value: expires_in);
   }
- 
- getToken(){
-  return _storage.read(key: 'token');
- }
+
+  Future<Map<String, String>> getToken() {
+    return _storage.readAll();
+  }
 }
